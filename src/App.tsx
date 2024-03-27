654861e42d245f6791602d632ff592bfd8d4edfc
Ahmad Ihsan <ihsanashi@gmail.com>
2024-03-27 22:04:00 +0800

    chore: install, setup and run prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import './App.css'
+import './App.css';
 
 function App() {
-
-
-  return (
-    <h1 className='text-3xl font-bold text-red-500'>Markdown Editor</h1>
-  )
+  return <h1 className="text-3xl font-bold text-red-500">Markdown Editor</h1>;
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
 import '@radix-ui/themes/styles.css';
 import { Theme } from '@radix-ui/themes';
@@ -11,5 +11,5 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <Theme>
       <App />
     </Theme>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
