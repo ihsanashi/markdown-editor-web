91bddfda56586b195bec88260005b59c2ea42ff8
Ahmad Ihsan <ihsanashi@gmail.com>
2024-03-28 13:15:12 +0800

    feat: setup basic 2 column layout

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,4 +1,5 @@
 import { createLazyFileRoute } from '@tanstack/react-router';
+import { Box, Grid, ScrollArea, TextArea } from '@radix-ui/themes';
 
 export const Route = createLazyFileRoute('/')({
   component: Index,
@@ -6,8 +7,20 @@ export const Route = createLazyFileRoute('/')({
 
 function Index() {
   return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
-    </div>
+    <Box width="98%" className="p-2.5">
+      <Grid columns="2" gap="2" className="divide-x">
+        <Box>
+          {/* Temporary usage until markdown editor is installed */}
+          <ScrollArea scrollbars="vertical" type="scroll">
+            <TextArea placeholder="Enter your content here..." />
+          </ScrollArea>
+        </Box>
+        <Box className="pl-2">
+          <ScrollArea scrollbars="vertical" type="scroll">
+            <h1>Preview area</h1>
+          </ScrollArea>
+        </Box>
+      </Grid>
+    </Box>
   );
 }
